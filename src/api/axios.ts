5f633286c563a86985a29dc44f14b45def672ade
cyn <jamess.raynorr@gmail.com>
2024-07-29 22:45:57 -0400

    ...

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 //setups up dotenv
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:6968',
+    baseURL: 'http://vos.wtf:6968',
     headers: {
         'Content-Type': 'application/json',
     },
